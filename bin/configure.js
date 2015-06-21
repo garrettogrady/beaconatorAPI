181ce17195837d3db3ed8c7edabbe7c1191daa62
Karl Swedberg <kswedberg@gmail.com>
2015-06-21 12:48:48 -0500

    Improve bin/config.js

--- bin/configure.js
+++ bin/configure.js
@@ -1,42 +1,46 @@
-// jshint ignore: start
-
+var promise = require('bluebird');
+var fs = promise.promisifyAll(require('fs'));
 var prompt = require('prompt');
-var fs = require('fs');
-// Remove annoying "prompt:"
-prompt.message = "";
-prompt.delimiter = "";
+
+
+// Remove annoying 'prompt:''
+prompt.message = '';
+prompt.delimiter = '';
 
 var apiGenKey = function(length) {
-    // Just produces random string using these chars
-    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-    return result;
-}
+  // Just produces random string using these chars
+  var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = length; i > 0; --i) {
+    result += chars[Math.round(Math.random() * (chars.length - 1))];
+  }
+
+  return result;
+};
 
 
 
 
 var messages = {
-  app: "Configuring Basic App Info...\n".green,
-  db: "Configuring MongoDB Connection...\n".green,
-  api: "Configuring API Server...\n".green,
-  gui: "Configuring GUI Server...\n".green,
-  email: "Configuring Email app uses...\n".green,
-}
+  app: 'Configuring Basic App Info...\n'.green,
+  db: 'Configuring MongoDB Connection...\n'.green,
+  api: 'Configuring API Server...\n'.green,
+  gui: 'Configuring GUI Server...\n'.green,
+  email: 'Configuring Email app uses...\n'.green,
+};
 
-// Define what we what entered
+// Define what we want entered
 var schema = {
   properties: {
     appName: {
-      description: messages.app+'App Name:'.blue,
-      default: 'Hapi Dash'
+      description: messages.app + 'App Name:'.blue,
+      default: 'Beaconator'
     },
     appUrl: {
       description: 'App Url:'.blue,
       default: 'http://localhost:3030',
       before: function removeLastSlash (url) {
-          if (url.substring(url.length-1) == "/")
+          if (url.substring(url.length-1) === '/')
           {
               url = url.substring(0, url.length-1);
           }
@@ -45,7 +49,7 @@ var schema = {
       }
     },
     apiHost: {
-      description: messages.api+'API host:'.blue,
+      description: messages.api + 'API host:'.blue,
       default: '127.0.0.1'
     },
     apiPort: {
@@ -60,14 +64,14 @@ var schema = {
       description: 'API Core Password:'.blue,
       default: 'random string',
       before: function(value) {
-        if(value === "random string") {
-          value = apiGenKey(64)
+        if(value === 'random string') {
+          value = apiGenKey(64);
         }
         return value;
       }
     },
     guiHost: {
-      description:  messages.gui+'GUI host:'.blue,
+      description:  messages.gui + 'GUI host:'.blue,
       default: '0.0.0.0'
     },
     guiPort: {
@@ -75,7 +79,7 @@ var schema = {
       default: '3030'
     },
     dbHost: {
-      description: messages.db+'Mongo host:'.blue,
+      description: messages.db + 'Mongo host:'.blue,
       default: '127.0.0.1'
     },
     dbPort: {
@@ -83,20 +87,20 @@ var schema = {
       default: '27017'
     },
     dbName: {
-      description: "Mongo db name:".blue,
-      default: 'hapi-dash'
+      description: 'Mongo db name:'.blue,
+      default: 'beacon'
     },
     dbUn: {
-      description: "Mongo db username (optional):".blue,
+      description: 'Mongo db username (optional):'.blue,
       default: ''
     },
     dbPw: {
-      description: "Mongo db password (optional):".blue,
+      description: 'Mongo db password (optional):'.blue,
       default: ''
     },
     mailService: {
-      description: messages.email+'Email service:'.blue,
-      default: 'Gmail'
+      description: messages.email + 'Email service:'.blue,
+      default: 'Mailgun'
     },
     mailEmail: {
       description: 'Email address:'.blue,
@@ -105,11 +109,57 @@ var schema = {
     mailPass: {
       description: 'Email password:'.blue,
       required: true,
-      message: "Password is required",
+      message: 'Password is required',
       hidden: true
     },
   }
 };
+
+var copyPm2 = function copyPm2() {
+  var pm2 = {
+    example: {
+      path: __dirname + '/../config.pm2-example.json',
+      file: 'config.pm2-example.json'
+    },
+    config: {
+      path: __dirname + '/../config.pm2.json',
+      file: 'config.pm2.json'
+    },
+  };
+  promise.all([
+    fs.statAsync(pm2.example.path),
+    fs.statAsync(pm2.config.path),
+  ])
+  .then(function(stats) {
+    // If No example file
+    if (!stats[0].isFile()) {
+
+      return false;
+    }
+
+    // If config.pm2.json already exists, do not overwrite it.
+    if (stats[1].isFile()) {
+      console.log('Skpping copy. The file'.yellow, pm2.config.file.cyan, 'already exists.'.yellow);
+      return false;
+    }
+  })
+  .catch(function(err) {
+    if (err.path === pm2.example.path) {
+      return console.log('Can\'t copy file.' + pm2.example.file + 'does not exist'.red);
+    }
+
+    fs.readFileAsync(pm2.example.path, {encoding: 'utf8'})
+    .then(function(content) {
+      return fs.writeFileAsync(pm2.config.path, content);
+    })
+    .then(function() {
+      console.log('Copied'.green, pm2.example.file.cyan, 'to'.green, pm2.config.file.cyan);
+    });
+
+  });
+};
+
+
 //
 // Start the prompt
 //
@@ -117,10 +167,14 @@ prompt.start();
 
 
 prompt.get(schema, function (err, result) {
+  if (err) {
+    console.log('prompt error');
+    console.log(err);
+  }
   //
   // Log the results.
   //
-  if(result) {
+  if (result) {
     var config = {
       app: {
         name: result.appName,
@@ -138,40 +192,72 @@ prompt.get(schema, function (err, result) {
         port: result.apiPort
       },
       coreCreds: {
-          id: result.apiCoreId,
-          key: result.apiCorePass,
-          algorithm: 'sha256'
+        id: result.apiCoreId,
+        key: result.apiCorePass,
+        algorithm: 'sha256'
       },
       gui: {
         host: result.guiHost,
         port: result.guiPort
       },
       email: {
-          service: result.mailService,
-          auth: {
-              user: result.mailEmail,
-              pass: result.mailPass
-          }
+        service: result.mailService,
+        auth: {
+          user: result.mailEmail,
+          pass: result.mailPass
+        }
       }
-    }
+    };
 
-    // Text to save to config.js
-    var text = "module.exports = "+JSON.stringify(config, null, 4)
-    fs.exists(__dirname+'/../config.js', function (exists) {
 
-      if(exists) {
-        fs.renameSync(__dirname+'/../config.js', __dirname+'/../config.old.js')
-        console.log('Backed up old config to config.old.js');
+    // Copy
+    // Save config info to config.js
+    var content = 'module.exports = ' + JSON.stringify(config, null, 2);
+    var configPath = __dirname + '/../config.js';
+    fs.statAsync(configPath)
+    .then(function(stat) {
+
+      // config.js exists, so rename before writing to it.
+      if (stat && stat.isFile()) {
+        return fs.renameAsync(configPath, __dirname + '/../config.old.js');
       }
 
-      fs.writeFile(__dirname+'/../config.js', text, function (err) {
-        if (err) return console.log(err);
-        console.log('Saved configuration: ', config);
-      });
+    })
+    .then(function(obj, b) {
+      // console.log('Backed up old config to config.old.js');
+      console.log('after rename. a:');
+      console.log(obj);
+      console.log('b');
+      console.log(b);
 
+      return fs.writeFileAsync(configPath, content);
+    })
+    .then(function(err) {
+      console.log('Saved configuration: '.green);
+      console.log(config);
+      copyPm2();
+    })
+    .catch(function(err) {
+
+      // config.js does not exist yet, so create it.
+      if (err.path === configPath) {
+        return fs.writeFileAsync(configPath, content)
+        .then(function() {
+          console.log('Saved configuration: '.green);
+          console.log(config);
+          copyPm2();
+        });
+      }
+
+      console.log('Error adding config.js:');
+      console.log(err);
+      console.log('Consider adding this file manually.');
     });
+
+
   } else {
-    console.log("\nAborted configuration!")
+    console.log('\nAborted configuration!');
   }
 
 });
+
