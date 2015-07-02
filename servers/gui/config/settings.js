0c6d25de84cee727441efb10fc8665d436bd2bb5
Karl Swedberg <kswedberg@gmail.com>
2015-07-02 15:11:48 -0400

    white space

--- servers/gui/config/settings.js
+++ servers/gui/config/settings.js
@@ -1,6 +1,7 @@
 /**
 * Dependencies.
 */
+
 var path = require('path'),
 rootPath = path.normalize(__dirname + '/../../..');
 
@@ -9,7 +10,9 @@ var config = require(path.join(rootPath, 'config'));
 // Defaults that you can access when you require this config.
 module.exports = {
   root: rootPath,
-  port: parseInt(process.env.PORT, 10) || config.gui.port, //was 3020
+
+  // port was 3020
+  port: parseInt(process.env.PORT, 10) || config.gui.port,
   hapi: {
     views: {
       path: './servers/gui/views',
